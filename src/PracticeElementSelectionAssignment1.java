511d76395f1940ac6c1ebcb19574c52e46524aaf
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-28 15:45:04 -0800

    practiced targeting checking and unchecking required check box and testing if it has been checked and unchecked, also practiced printing to the console how many checkboxes were available on the webpage

---
+++ src/PracticeElementSelectionAssignment1.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.asserts.Assertion;
+
+public class PracticeElementSelectionAssignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://qaclickacademy.com/practice.php");
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		driver.close();
+	}
+
+}
